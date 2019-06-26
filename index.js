2ebcb1462e7d42b685e0915c3b65f32030c7520a
Ivan5 <converse_ivan@hotmail.com>
2019-06-26 15:12:26 -0500

    Make database models

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => {
-      return "Hello World";
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
     }
   }
 };
